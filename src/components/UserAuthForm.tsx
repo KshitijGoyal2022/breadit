639ede3b9e126b1a5f5984dbfdc389500586336c
Kshitij Goyal <117494616+KshitijGoyal2022@users.noreply.github.com>
2024-04-21 22:41:12 -0700

    feature: authentication

--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -1,13 +1,39 @@
+'use client'
+
 import { cn } from '@/lib/utils';
 import { Button } from './ui/Button';
+import { useState } from 'react';
+import { signIn } from 'next-auth/react';
+import { Icons } from './Icons';
+import { toast } from '@/hooks/use-toast';
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const UserAuthForm = ({ className, ...props }: UserAuthFormProps) => {
 
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const loginWithGoogle = async()=>{
+    setIsLoading(true);
+    try {
+      await signIn('google')
+    } catch (error) {
+      toast({
+        title:'There was a problem.',
+        description: 'There was a problem logging in with Google.',
+        variant: 'destructive'
+      })
+    }
+    finally{
+      setIsLoading(false);
+    }
+
+  }
+
   return (
     <div className={cn('flex justify-center', className)} {...props}>
-      <Button size='sm' className='w-full'>
+      <Button onClick={loginWithGoogle} isLoading={isLoading} size='sm' className='w-full'>
+        {isLoading ? null: <Icons.google className= 'h-4 w-4 mr-2' />}
         Google
       </Button>
     </div>
