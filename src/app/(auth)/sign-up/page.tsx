bbb98b0e873dea5b04b24b3395afa5408b98b9b5
Kshitij Goyal <117494616+KshitijGoyal2022@users.noreply.github.com>
2024-07-01 17:36:11 -0700

    interception modal

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,4 +1,5 @@
-import SignIn from '@/components/sign-in';
+
+import SignUp from '@/components/sign-up';
 import { buttonVariants } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 import { ChevronLeft } from 'lucide-react';
@@ -14,7 +15,7 @@ const page: FC = () => {
           Home
         </Link>
 
-        <SignIn />
+        <SignUp />
       </div>
     </div>
   );
